a4b1353741fde100c2c73d3b00fcddb671643fb2
theerawatthongriem <theerawatthongriem@gmail.com>
2023-07-04 14:29:02 +0700

    commit

--- myweb/settings.py
+++ myweb/settings.py
@@ -127,10 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
