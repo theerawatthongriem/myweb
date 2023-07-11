ed1d25392371c06ec61e336332183547180a07eb
theerawatthongriem <theerawatthongriem@gmail.com>
2023-07-11 13:15:45 +0700

    commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Profile
+from .models import *
 
 admin.site.register(Profile)
+admin.site.register(Faculty)
+admin.site.register(Branch)
--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,22 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+class Faculty(models.Model):
+    faculty_name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f'{self.faculty_name}'
+    
+class Branch(models.Model):
+    faculty_name = models.ForeignKey(Faculty,on_delete=models.CASCADE)
+    branch_name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f'{self.branch_name}'
+
+# class Predict(models.Model):
+#     stid = models.IntegerField(max_length=15)
+#     parent_state = models.CharField(max_length=100)
+#     urban_school = models.CharField(max_length=100)
+#     home_province = models.CharField(max_length=100)
+#     urban_home = models.CharField(max_length=100)
